3d426edf5979fa27da95850caabc7fca062caf1b
Eff0rtless <63397793+TTujh@users.noreply.github.com>
2024-07-10 12:43:44 +0700

    Update scanner.py

--- scanner/scanner.py
+++ scanner/scanner.py
@@ -1,43 +1,44 @@
-import time
-
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QCheckBox, QLineEdit, QGridLayout, QPushButton, QMessageBox
-from PyQt6.QtCore import Qt
 import sys
 import serial
 import os
 import configparser
 import threading
-import sqlConnect
+import sql_connection
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QGridLayout, QPushButton, QMessageBox
+from PyQt6.QtCore import Qt
 
 
 class MainWindow(QWidget):
+    ini_port: serial.Serial
 
-    def __init__(self):
-
+    def __init__(self) -> None:
         super().__init__()
-        self.initializeUI()
+        self.initialize_ui()
         self.flag = True
-        self.config = self.readConfig()
+        self.config = self.read_config()
         self.all_scanned_codes = set()
-        self.isIniExists()
-        self.startScanning()
+        self.is_ini_exists()
+        self.start_scanning()
 
-    def initializeUI(self):
-
+    def initialize_ui(self) -> None:
         self.setFixedSize(700, 150)
         self.setStyleSheet('background-color: grey')
         self.setWindowTitle('Symbol Bar Code Scanner')
-        self.setUpWindow()
+        self.set_up_window()
         self.show()
 
-    def readConfig(self):
-
+    def read_config(self) -> tuple:
         path = os.getcwd() + '/settings.ini'
         config = configparser.ConfigParser()
         config.read(path)
+        self.__dbname = config.get("DataBase", "dbname")
+        self.__user = config.get("DataBase", "user")
+        self.__password = config.get("DataBase", "password")
+        self.__host = config.get("DataBase", "host")
+        self.__port = config.get("DataBase", "port")
         return config.get("COM", "path"), config.get("BAUD", "baudrate")
 
-    def initializePort(self):
+    def initialize_port(self) -> None:
         self.ini_port = serial.Serial(self.config[0], baudrate=int(self.config[1]))
         while self.ini_port.is_open:
             data = self.ini_port.readline().decode().strip()
@@ -45,9 +46,7 @@ class MainWindow(QWidget):
                 self.scanner_field.setText(data)
                 self.all_scanned_codes.add(data)
 
-
-    def setUpWindow(self):
-
+    def set_up_window(self) -> None:
         self.grid = QGridLayout()
 
         self.lable = QLabel('<b>Данные с штрих-кода</b>')
@@ -55,8 +54,6 @@ class MainWindow(QWidget):
         self.grid.addWidget(self.lable, 0, 1, 1, 2)
 
         self.scanner_field = QLineEdit()
-        # Надо проверить, чтобы писать нельзя было, а копировать можно из поля.
-        # self.scanner_field.setDisabled(True)
         self.grid.addWidget(self.scanner_field, 1, 0, 1, 4)
 
         self.start_button = QPushButton('Сброс')
@@ -66,43 +63,47 @@ class MainWindow(QWidget):
 
         self.clipboard = QPushButton('Скопировать')
         self.clipboard.setStyleSheet('background-color: #808080')
-        self.clipboard.clicked.connect(self.clipBoardCopy)
+        self.clipboard.clicked.connect(self.clipboard_copy)
         self.grid.addWidget(self.clipboard, 2, 1)
 
         self.showInformation = QPushButton('Информация о коде')
         self.showInformation.setStyleSheet('background-color: #808080')
-        self.showInformation.clicked.connect(self.grabFromDB)
+        self.showInformation.clicked.connect(self.grab_from_database)
         self.grid.addWidget(self.showInformation, 2, 2)
 
         self.exit_button = QPushButton('Выход')
         self.exit_button.setStyleSheet('background-color: red')
-        self.exit_button.clicked.connect(self.closeApplication)
+        self.exit_button.clicked.connect(self.close_application)
         self.grid.addWidget(self.exit_button, 2, 3)
 
         self.setLayout(self.grid)
 
-    def closeApplication(self):
+    def close_application(self) -> None:
         self.ini_port.close()
         self.closeEvent()
 
-    def startScanning(self):
-
-        self.process = threading.Thread(target=self.initializePort)
+    def start_scanning(self) -> None:
+        self.process = threading.Thread(target=self.initialize_port)
         self.process.daemon = True
         self.process.start()
 
-    def clipBoardCopy(self):
+    def clipboard_copy(self) -> None:
         if self.scanner_field.text() != "":
             clipboard = QApplication.clipboard()
             clipboard.setText(f"{self.scanner_field.text()}")
 
-    def grabFromDB(self):
-        if len(self.scanner_field.text()):
+    def grab_from_database(self) -> None:
+        if self.scanner_field.text():
+            database = sql_connection.RemotePostgresDatabase(dbname=self.__dbname,
+                                                             user=self.__user,
+                                                             password=self.__password,
+                                                             host=self.__host,
+                                                             port=self.__port)
             QMessageBox.information(self, "Сопутсвующие характеристики кода",
-                                    sqlConnect.connectToSendData((self.scanner_field.text())),
+                                    database.get_answer(self.scanner_field.text().encode()),
                                     QMessageBox.StandardButton.Ok)
 
-    def isIniExists(self):
+    def is_ini_exists(self) -> None:
         try:
             ini = serial.Serial(self.config[0], baudrate=int(self.config[1]))
             ini.close()
