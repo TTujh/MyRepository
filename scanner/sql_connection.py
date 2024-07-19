0feb0dbe79ec1e3ae68971434376fe22653ef56a
Eff0rtless <63397793+TTujh@users.noreply.github.com>
2024-07-19 10:34:19 +0700

    Update sql_connection.py

--- scanner/sql_connection.py
+++ scanner/sql_connection.py
@@ -1,51 +1,38 @@
 """Module sql_connection response for connection to local/remote SQL database"""
 
-import psycopg2 as psql
 import datetime
+import psycopg2 as psql
+from dataclasses import dataclass
 
 
-class RemotePostgresDatabase:
-    connection: psql.connect
+@dataclass
+class Connection:
+    dbname: str
+    user: str
+    password: str
+    host: str
+    port: str
 
-    def __init__(self, dbname: str, user: str, password: str, host: str, port: str) -> None:
-        self.connection = psql.connect(dbname=dbname,
-                                       user=user,
-                                       password=password,
-                                       host=host,
-                                       port=port)
 
-        self._information, self._columns = [], []
-        self.cursor = self.connection.cursor()
+class PostgresData(Connection):
 
-    def _get_code_information(self, code: bytes) -> None:
-        code = code.decode('unicode_escape').encode()
-        self.cursor.execute(
-            f"SELECT DISTINCT * FROM codes_input i LEFT JOIN codes_input_ext e ON i.id = e.codes_input_id WHERE code_hex = '{code.hex()}';"
-        )
-        self._put_code_information()
-
-        self.cursor.execute(
-            f"SELECT DISTINCT * FROM codes_output WHERE code_hex = '{code.hex()}';"
-        )
-        self._put_code_information()
-
-    def _put_code_information(self) -> None:
-        fetchall = self.cursor.fetchall()
-        if len(fetchall) != 0:
-            self._columns.append([desc[0] for desc in self.cursor.description])
-            self._information.append(fetchall)
-            self.cursor.execute(f"SELECT * FROM products WHERE id = '{self._information[0][0][3]}';")
-            self._columns.append([desc[0] for desc in self.cursor.description])
-            self._information.append(self.cursor.fetchall())
-
-    def get_answer(self, code: bytes) -> str:
-        self._get_code_information(code=code)
-        answer = 'ИНФОРМАЦИЯ О КОДЕ:\n'
-        if len(self._columns) != 0:
-            for i in range(len(self._columns[0])):
-                if self._information[0][0][i]:
-                    answer += f'{self._columns[0][i]} — {self._information[0][0][i]}\n'
-        else:
-            answer += 'ИНФОРМАЦИЯ ОТСУТСВУЕТ В ВЫБРАННОЙ БАЗЕ ДАННЫХ.\n'
-        answer += '----------------------------------------------------------------------------'
+    def fetch_data_from_sql_table(self, code: bytes) -> str:
+        connection = psql.connect(dbname=self.dbname, user=self.user, password=self.password, host=self.host,
+                                  port=self.port)
+        code = code.decode('unicode-escape').encode()
+        cursor = connection.cursor()
+        answer = "CODE INPUT:\n"
+        cursor.execute("SELECT * FROM codes_input WHERE code_hex= '%s';" % code.hex())
+        input_codes = cursor.fetchone()
+        input_codes_desc = cursor.description
+        cursor.execute("SELECT * FROM codes_output WHERE code_hex = '%s';" % code.hex())
+        output_codes = cursor.fetchone()
+        output_codes_desc = cursor.description
+        for i in range(1, len(input_codes)):
+            answer += f"\t{input_codes_desc[i][0]} --- {input_codes[i]}\n"
+        answer += "CODE OUTPUT:\n"
+        for i in range(1, len(output_codes)):
+            answer += f"\t{output_codes_desc[i][0]} --- {output_codes[i]}\n"
+        cursor.close()
+        connection.close()
         return answer
